e3343939545786344b615af19257dcf3f69249c8
sujantechprofacc01-cmd <sujantechprofacc01@gmail.com>
2026-01-02 18:35:54 +0530

    Update Sample2.java

--- src/test/java/mavenpractice/Sample2.java
+++ src/test/java/mavenpractice/Sample2.java
@@ -1,1 +1,8 @@
-
+public class GithubPracticeTest {
+      @Test
+      public void githubPracticeTest() {
+        System.out.println("hii github");
+    	String data = System.getProperty("smarty");
+    	System.out.println(data);
+      }
+}
